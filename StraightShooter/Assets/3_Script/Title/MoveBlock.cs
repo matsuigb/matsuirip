a5f66cf1fe009a27fd50618daea90d8caa886803
matsuigb <33863203+matsuigb@users.noreply.github.com>
2018-01-08 19:32:59 +0900

    Add files via upload

---
+++ StraightShooter/Assets/3_Script/Title/MoveBlock.cs
@@ -0,0 +1,50 @@
+﻿// タイトル画面の動くブロック
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBlock : MonoBehaviour
+{
+    private float m_angle;            // 角度(位置を決める)
+    private float m_moveSpeed;        // 移動速度
+    private float m_radius;                // 移動する円の半径
+
+    private float ANGLE_MAX = 359.99f;
+    private float SPEED_RATE = 0.15f;
+
+    // 各半径倍率
+    private float RADIUS_RATE1 = 10.0f;
+    private float RADIUS_RATE2 = 20.0f;
+    private float RADIUS_RATE3 = 24.0f;
+    private float RADIUS_RATE4 = 28.0f;
+    private float RADIUS_RATE5 = 32.0f;
+    // 追加半径距離
+    private float ADD_RADIUS = 5.0f;
+
+    void Start()
+    {
+        m_angle = Random.value * ANGLE_MAX;
+        m_moveSpeed = SPEED_RATE * Random.value; 
+
+        float f = Random.value;
+
+        if (f < 0.2f) m_radius = RADIUS_RATE1 * f;
+        else if (f < 0.4f) m_radius = RADIUS_RATE2 * f;
+        else if (f < 0.6f) m_radius = RADIUS_RATE3 * f;
+        else if (f < 0.8f) m_radius = RADIUS_RATE4 * f;
+        else m_radius = RADIUS_RATE5 * f;
+        m_radius += ADD_RADIUS;
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        m_angle += m_moveSpeed * Time.deltaTime;
+        transform.position = new Vector3(Mathf.Cos(m_angle) * m_radius, transform.position.y, Mathf.Sin(m_angle) * m_radius);
+    }
+}
---
+++ StraightShooter/Assets/3_Script/Title/TitleManager.cs
@@ -0,0 +1,60 @@
+﻿// タイトルマネージャ
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_moveBlock;
+    [SerializeField]
+    private GameObject m_moveBlock2;
+    [SerializeField]
+    private GameObject m_parentObj;
+
+    private AudioSource m_se;
+    bool m_sceneChangeFlg = false;
+    float m_cnt;
+    const float CHANGE_SCENE_TIME = 3.5f;
+
+    private const int MAKE_BLOCK_VAL = 512;
+
+    void Start()
+    {
+        m_cnt = 0.0f;
+        m_se = GetComponent<AudioSource>();
+
+        for (int i = 0; i < MAKE_BLOCK_VAL; i++)
+        {
+            if (i < MAKE_BLOCK_VAL/2)
+            {
+                Instantiate(m_moveBlock, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, m_parentObj.transform);
+            }
+            else
+            {
+                Instantiate(m_moveBlock2, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity, m_parentObj.transform);
+            }
+        }
+    }
+
+    void Update()
+    {
+        // シーン遷移
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            m_sceneChangeFlg = true;
+            m_se.PlayOneShot(m_se.clip);
+        }
+
+        if (m_sceneChangeFlg)
+        {
+            m_cnt += Time.deltaTime;
+            if(m_cnt>= CHANGE_SCENE_TIME)
+            {
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+}
